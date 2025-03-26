ee41de07d29ca80ea63ab73a279ba59d245db46a
simoneburrai <burrai.s98@gmail.com>
2025-03-26 16:27:49 +0100

    feat: added app.listen to start server, and app.send to send response to / route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("ciao");
+const express = require("express");
+const app = express();
+const port = 2000;
+
+
+app.get("/", (req, res) => {
+res.send("Server del mio Blog");
+})
+
+
+
+app.listen(2000, () => {
+	console.log(`Server attivo nella porta ${port}`);
+});
